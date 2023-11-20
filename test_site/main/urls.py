c6095cf37fb319edfde660ecca40f5705a22d4b9
Daniil <mr.daniil00@bk.ru>
2023-11-20 13:40:26 +0700

    add urls for serv and cont

--- test_site/main/urls.py
+++ test_site/main/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),
+    path('servs', views.serv),
+    path('cont', views.cont),
+]
     ]
--- test_site/main/views.py
+++ test_site/main/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main/index.html')
+
+
+def serv(request):
+    return render(request, 'main/serv.html')
+
+
+def cont(request):
+    return render(request, 'main/cont.html')
